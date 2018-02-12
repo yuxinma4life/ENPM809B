579e413729cc1678e3930c874c64cdd0ff1fecf4
zzjkf2009 <zzeng@terpmail.umd.edu>
2018-02-12 16:34:21 -0500

    Update the ducomentation for source file

--- src/turtleGo_node.cpp
+++ src/turtleGo_node.cpp
@@ -1,29 +1,65 @@
+/**
+ * @Author: Zejiang Zeng , Pranav Dhulipala, Shaotu Jia, Yuxin Ma, Ori Perl
+ * @Date:   2018-02-12T15:47:28-05:00
+ * @Email:  zzeng@terpmail.umd.edu
+ * @Last modified time: 2018-02-12T16:33:34-05:00
+ * @License: BSD
+ * @Brief: This program publishes randomly-generated velocity messages for turtlebot
+ */
+
+
+
 #include "ros/ros.h"
-#include "geometry_msgs/Twist.h"
+#include "geometry_msgs/Twist.h" // For geometry_msgs::Twist
+#include <stdlib.h> // For rand() and RAND_MAX
 
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "turtleGo_node");
-  ros::NodeHandle nh;
-  ros::Publisher vel_pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/teleop",100);
-  geometry_msgs::Twist vel;
-  vel.linear.x = 0.5;
-  vel.linear.y = 0.5;
-  vel.angular.z = 0.2;
+        // Initialize the ROS system and become a node.
+        ros::init(argc, argv, "turtleGo_node");
+        ros::NodeHandle nh;
+        /**The work of actually publishing the messages is done by an object of class ros::Publisher
+            Create a publisher object.
 
-  ros::Rate loop_rate(10);
-  while (ros::ok())
-  {
+           The node_handle is an object of class ros::NodeHandle, one that you created near the start of your program.
+           We’re calling the advertise method of that object.
 
-    //ROS_INFO_STREAM("I'm saying" <<vel.x);
+           The message_type part inside the angle brackets—formally called the template parameter—is the data type for the
+           messages we want to publish. This should be the name of the class defined in the header discussed above.
+           In the example, we use the geometry_msgs::Twist class.
 
-    vel_pub.publish(vel);
+           The last parameter to advertise is an integer representing the size of the message queue
+           for this publisher. In most cases, a reasonably large value, say 100, is suitable.
+           If your program rapidly publishes more messages than the queue can hold, the
+           oldest unsent messages will be discarded.
+         **/
+        ros::Publisher vel_pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/teleop",100);
+        // set up linear and angular velocity for turtlebot
+        geometry_msgs::Twist vel;
+        vel.linear.x = 0.5;
+        vel.angular.z = 0.2;
+        /**
+         * OR generate random velocity
+         */
+        // Seed the random number genertor
+        //  srand(time(0));
 
-    ros::spinOnce();
+        // Loop at 10Hz until the node is shut down.
+        ros::Rate loop_rate(10);
+        while(ros::ok()) {
 
-    loop_rate.sleep();
-  }
+                //  vel.linear.x = double(rand())/double(RAND_MAX);
+                //  vel.angular.z = 2*double(rand())/double(RAND_MAX) - 1;
 
-  return 0;
+
+                // publish the message
+                vel_pub.publish(vel);
+
+                ros::spinOnce();
+
+                loop_rate.sleep();
+        }
+
+        return 0;
 }
